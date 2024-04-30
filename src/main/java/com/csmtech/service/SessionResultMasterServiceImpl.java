5fe1234737512570ee1aefa1e15db49424c386bd
akshaykirti <akshaykirti1234@gmail.com>
2024-04-30 09:51:58 +0530

    conflict resolved

--- src/main/java/com/csmtech/service/SessionResultMasterServiceImpl.java
+++ src/main/java/com/csmtech/service/SessionResultMasterServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.csmtech.dto.SessionResultDto;
 import com.csmtech.entity.SessionAssessmentMaster;
@@ -22,10 +23,25 @@ public class SessionResultMasterServiceImpl implements SessionResultMasterServic
 	private SessionAssessmentMasterRepository sessionAssessmentMasterRepository;
 
 	@Override
+	@Transactional
 	public ResponseEntity<?> saveSessionResult(List<SessionResultDto> responsePayload) {
 
 		Integer userId = 0;
 		Integer sessionId = 0;
+
+		for (SessionResultDto value : responsePayload) {
+			// Retrieve sessionAssessmentMaster from repository
+			SessionAssessmentMaster sessionAssessmentMaster = sessionAssessmentMasterRepository
+					.findById(value.getSessionAssessmentMasterId())
+					.orElseThrow(() -> new RuntimeException("Session Assessment Master not found"));
+
+			// get userId and sessionId
+			userId = value.getUserId();
+			sessionId = sessionAssessmentMaster.getSessionMaster().getSessionId();
+
+//			Delete sessionResultMaster by userId and sessionId
+			sessionResultMasterRepository.deleteResultBySessionIdAndUserId(sessionId, userId);
+		}
 
 		for (SessionResultDto value : responsePayload) {
 			// Retrieve sessionAssessmentMaster from repository
@@ -57,9 +73,6 @@ public class SessionResultMasterServiceImpl implements SessionResultMasterServic
 				sessionResultMaster.setResultStatus(false);
 
 			}
-
-			// Delete sessionResultMaster by userId and sessionId
-//			sessionResultMasterRepository.deleteResultBySessionIdAndUserId(sessionId, userId);
 
 			// Save sessionResultMaster to database
 			SessionResultMaster save = sessionResultMasterRepository.save(sessionResultMaster);
