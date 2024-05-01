7919cab8b93218c1737e93b0ded50f09c6222034
Priyanka priyadarsini Biswal <priyanka.biswal@csm.tech>
2024-05-01 10:54:39 +0530

    some modification

--- src/main/java/com/csmtech/service/AssessmentMasterServiceImpl.java
+++ src/main/java/com/csmtech/service/AssessmentMasterServiceImpl.java
@@ -1,12 +1,23 @@
 package com.csmtech.service;
 
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.csmtech.dto.AssessmentMasterDto;
 import com.csmtech.entity.AssessmentMaster;
@@ -30,122 +41,23 @@ public class AssessmentMasterServiceImpl implements AssessmentMasterService {
 
 	@Override
 	public AssessmentMaster saveAssessment(AssessmentMasterDto assessmentDto) {
-//		try {
-//			logger.info("saveAssessment method of AssessmentMasterServiceImpl is executed");
-//			if (assessmentDto.getSessionId() != null) {
-//				// If sessionId is present, save data in SessionAssessmentMaster
-//				SessionAssessmentMaster sessionSave = new SessionAssessmentMaster();
-////	            sessionSave.setSessionAssesmentMasterId(assessmentDto.getAssessmentId());
-////	            sessionSave.setModuleId(assessmentDto.getModuleId());
-////	            sessionSave.setSubmoduleId(assessmentDto.getSubmoduleId());
-////	            sessionSave.setScheduleForId(assessmentDto.getScheduleForId());
-////	            sessionSave.setSessionId(assessmentDto.getSessionId());
-//				sessionSave.setQuestion(assessmentDto.getQuestion());
-//				sessionSave.setOption1(assessmentDto.getOption1());
-//				sessionSave.setOption2(assessmentDto.getOption2());
-//				sessionSave.setOption3(assessmentDto.getOption3());
-//				sessionSave.setOption4(assessmentDto.getOption4());
-//				sessionSave.setAnswer(assessmentDto.getAnswer());
-////	            sessionSave.setCreatedBy(1);
-////	            sessionSave.setUpdatedBy(1);
-//				SessionAssessmentMaster sessionMasterSave = sessionAssessmentMasterRepository.save(sessionSave);
-//				// Check if assessment is saved successfully
-//				if (sessionMasterSave != null) {
-//					// Update flag in another table
-//					int updatedRows = scheduleForMasterRepository
-//							.updateFlagByScheduleId(assessmentDto.getScheduleForId());
-//					// Handle the update status if needed
-//				}
-//				return null; // Return null as SessionAssessmentMaster is being saved
-//			} else {
-//				// If sessionId is not present, save data in AssessmentMaster
-//				AssessmentMaster assessmentMaster = new AssessmentMaster();
-//				assessmentMaster.setAssessmentMasterId(assessmentDto.getAssessmentId());
-//				assessmentMaster.setModuleId(assessmentDto.getModuleId());
-//				assessmentMaster.setSubmoduleId(assessmentDto.getSubmoduleId());
-//				assessmentMaster.setScheduleForId(assessmentDto.getScheduleForId());
-//				assessmentMaster.setQuestion(assessmentDto.getQuestion());
-//				assessmentMaster.setOption1(assessmentDto.getOption1());
-//				assessmentMaster.setOption2(assessmentDto.getOption2());
-//				assessmentMaster.setOption3(assessmentDto.getOption3());
-//				assessmentMaster.setOption4(assessmentDto.getOption4());
-//				assessmentMaster.setAnswer(assessmentDto.getAnswer());
-//				assessmentMaster.setCreatedBy(1);
-//				assessmentMaster.setUpdatedBy(1);
-//				AssessmentMaster save = assessmentMasterRepository.save(assessmentMaster);
-//				// Check if assessment is saved successfully
-//				if (save != null) {
-//					// Update flag in another table
-//					int updatedRows = scheduleForMasterRepository
-//							.updateFlagByScheduleId(assessmentDto.getScheduleForId());
-//					// Handle the update status if needed
-//				}
-//				return save;
-//			}
-//		} catch (Exception e) {
-//			logger.info("Error occurred in saveAssessment method of AssessmentMasterServiceImpl: " + e.getMessage());
-//			e.printStackTrace();
-//			return null;
-//		}
-//		try {
-//			logger.info("saveAssessment method of AssessmentMasterServiceImpl is executed");
-//			if (assessmentDto.getSessionId() != null) {
-//				// If sessionId is present, save data in SessionAssessmentMaster
-//				SessionAssessmentMaster sessionSave = new SessionAssessmentMaster();
-//				sessionSave.setSessionAssesmentMasterId(assessmentDto.getAssessmentId());
-//				sessionSave.setModuleId(assessmentDto.getModuleId());
-//				sessionSave.setSubmoduleId(assessmentDto.getSubmoduleId());
-//				sessionSave.setScheduleForId(assessmentDto.getScheduleForId());
-//				sessionSave.setSessionId(assessmentDto.getSessionId());
-//				sessionSave.setQuestion(assessmentDto.getQuestion());
-//				sessionSave.setOption1(assessmentDto.getOption1());
-//				sessionSave.setOption2(assessmentDto.getOption2());
-//				sessionSave.setOption3(assessmentDto.getOption3());
-//				sessionSave.setOption4(assessmentDto.getOption4());
-//				sessionSave.setAnswer(assessmentDto.getAnswer());
-//				sessionSave.setCreatedBy(1);
-//				sessionSave.setUpdatedBy(1);
-//				SessionAssessmentMaster sessionMasterSave = sessionAssessmentMasterRepository.save(sessionSave);
-//				// Check if assessment is saved successfully
-//				if (sessionMasterSave != null) {
-//					// Update flag in another table
-//					int updatedRows = scheduleForMasterRepository
-//							.updateFlagByScheduleId(assessmentDto.getScheduleForId());
-//					// Handle the update status if needed
-//				}
-//				return null; // Return null as SessionAssessmentMaster is being saved
-//			} else {
-//				// If sessionId is not present, save data in AssessmentMaster
-//				AssessmentMaster assessmentMaster = new AssessmentMaster();
-//				assessmentMaster.setAssessmentMasterId(assessmentDto.getAssessmentId());
-//				assessmentMaster.setModuleId(assessmentDto.getModuleId());
-//				assessmentMaster.setSubmoduleId(assessmentDto.getSubmoduleId());
-//				assessmentMaster.setScheduleForId(assessmentDto.getScheduleForId());
-//				assessmentMaster.setQuestion(assessmentDto.getQuestion());
-//				assessmentMaster.setOption1(assessmentDto.getOption1());
-//				assessmentMaster.setOption2(assessmentDto.getOption2());
-//				assessmentMaster.setOption3(assessmentDto.getOption3());
-//				assessmentMaster.setOption4(assessmentDto.getOption4());
-//				assessmentMaster.setAnswer(assessmentDto.getAnswer());
-//				assessmentMaster.setCreatedBy(1);
-//				assessmentMaster.setUpdatedBy(1);
-//				AssessmentMaster save = assessmentMasterRepository.save(assessmentMaster);
-//				// Check if assessment is saved successfully
-//				if (save != null) {
-//					// Update flag in another table
-//					int updatedRows = scheduleForMasterRepository
-//							.updateFlagByScheduleId(assessmentDto.getScheduleForId());
-//					// Handle the update status if needed
-//				}
-//				return save;
-//			}
-//		} catch (Exception e) {
-//			logger.info("Error occurred in saveAssessment method of AssessmentMasterServiceImpl: " + e.getMessage());
-//			e.printStackTrace();
-//			return null;
-//		}
-		return null;
+		AssessmentMaster assMaster= new AssessmentMaster();
+		
+		assMaster.setAssessmentId(assessmentDto.getAssessmentId());
+		assMaster.setModuleId(assessmentDto.getModuleId());
+		assMaster.setSubmoduleId(assessmentDto.getSubmoduleId());
+		assMaster.setScheduleForId(assessmentDto.getScheduleForId());
+		assMaster.setQuestion(assessmentDto.getQuestion());
+		assMaster.setOption1(assessmentDto.getOption1());
+		assMaster.setOption2(assessmentDto.getOption2());
+		assMaster.setOption3(assessmentDto.getOption3());
+		assMaster.setOption4(assessmentDto.getOption4());
+		assMaster.setAnswer(assessmentDto.getAnswer());
+		assMaster.setCreatedBy(1);
+		assMaster.setUpdatedBy(1);
+		return assessmentMasterRepository.save(assMaster);
 	}
+	
 
 	@Override
 	public List<Map<String, Object>> viewAssessmentData() {
@@ -167,18 +79,116 @@ public class AssessmentMasterServiceImpl implements AssessmentMasterService {
 		return assessmentMasterRepository.updateAssessment(id);
 	}
 
+	
+	
+	// For upload excel
+	
+	
 	@Override
-	public List<Map<String, Object>> viewAssessmentForSessionData() {
-		return assessmentMasterRepository.viewAssessmentForSessionData();
+	public List<Map<String, Object>> retriveModuleTypeList() {
+		return assessmentMasterRepository.retriveModuleTypeList();
 	}
 
 	@Override
-	public void deleteAssessmentSession(Integer id) {
-		assessmentMasterRepository.deleteAssessmentSession(id);
+	public List<Map<String, Object>> retriveSubModuleList() {
+		return assessmentMasterRepository.retriveSubModuleList();
 	}
 
 	@Override
-	public Map<String, Object> getAssessmentSessionById(Integer id) {
-		return assessmentMasterRepository.getAssessmentSessionById(id);
+	public List<Map<String, Object>> retriveScheduleForList() {
+
+		return assessmentMasterRepository.retriveScheduleForList();
+	}
+
+
+
+	// For Module based Questions uplaod
+	@Override
+	public ResponseEntity<Map<String, Object>> uploadExcelData(MultipartFile file) {
+		Map<String, Object> responseMap = new HashMap<>();
+		try (Workbook workbook = new XSSFWorkbook(file.getInputStream())) {
+			Sheet sheet = workbook.getSheetAt(0); // Assuming data is in the first sheet
+
+			// Retrieve module, submodule, and schedulefor data from database
+			List<Map<String, Object>> moduleList = retriveModuleTypeList();
+			List<Map<String, Object>> subModuleList = retriveSubModuleList();
+			List<Map<String, Object>> scheduleForList = retriveScheduleForList();
+
+			// Iterate through rows to extract data and save to the database
+			for (int i = 1; i <= sheet.getLastRowNum(); i++) { // Assuming the first row is header
+				Row row = sheet.getRow(i);
+
+				// Check if the first cell of the row is null or empty, indicating an empty row
+				Cell firstCell = row.getCell(0);
+				if (firstCell == null || firstCell.getCellType() == CellType.BLANK) {
+					break; // Exit the loop if an empty row is encountered
+				}
+
+				AssessmentMaster assessmentMaster = new AssessmentMaster();
+
+				// Retrieve moduleId, submoduleId, and scheduleForId from Excel
+				String moduleType = getStringCellValue(row.getCell(0));
+				String submoduleName = getStringCellValue(row.getCell(1));
+				String scheduleFor = getStringCellValue(row.getCell(2));
+
+				// Find the corresponding IDs from the retrieved lists
+				int moduleId = findIdFromList(moduleType, moduleList, "MODULENAME", "MODULEID");
+				int submoduleId = findIdFromList(submoduleName, subModuleList, "SUBMODULENAME", "SUBMODULEID");
+				int scheduleForId = findIdFromList(scheduleFor, scheduleForList, "SCHEDULEFOR", "SCHEDULEFORID");
+
+				assessmentMaster.setModuleId(moduleId);
+				assessmentMaster.setSubmoduleId(submoduleId);
+				assessmentMaster.setScheduleForId(scheduleForId);
+
+				// Set other fields from Excel
+				assessmentMaster.setQuestion(getStringCellValue(row.getCell(3)));
+				assessmentMaster.setOption1(getStringCellValue(row.getCell(4)));
+				assessmentMaster.setOption2(getStringCellValue(row.getCell(5)));
+				assessmentMaster.setOption3(getStringCellValue(row.getCell(6)));
+				assessmentMaster.setOption4(getStringCellValue(row.getCell(7)));
+				assessmentMaster.setAnswer(getStringCellValue(row.getCell(8)));
+				assessmentMaster.setCreatedBy(1);
+				assessmentMaster.setUpdatedBy(1);
+
+				assessmentMasterRepository.save(assessmentMaster);
+			}
+			responseMap.put("success", true);
+			responseMap.put("message", "Data uploaded successfully.");
+			return ResponseEntity.ok(responseMap);
+		} catch (IOException e) {
+			e.printStackTrace();
+			responseMap.put("success", false);
+			responseMap.put("message", "Failed to upload data. Please try again.");
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseMap);
+		}
+	}
+
+
+	// Method to find ID from list based on name
+	private int findIdFromList(String name, List<Map<String, Object>> list, String nameKey, String idKey) {
+		for (Map<String, Object> item : list) {
+			if (name.equals(item.get(nameKey))) {
+				return (int) item.get(idKey);
+			}
+		}
+		return -1; // Return -1 if not found
+	}
+
+	private String getStringCellValue(Cell cell) {
+		if (cell != null) {
+			switch (cell.getCellType()) {
+			case STRING:
+				return cell.getStringCellValue();
+			case NUMERIC:
+				return String.valueOf(cell.getNumericCellValue());
+			case BOOLEAN:
+				return String.valueOf(cell.getBooleanCellValue());
+			case FORMULA:
+				return cell.getCellFormula(); // You may need to handle formula cells differently
+			default:
+				return ""; // Return empty string for other cell types
+			}
+		}
+		return ""; // Return empty string if cell is null
 	}
 }
